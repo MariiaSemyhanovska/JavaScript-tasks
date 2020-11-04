27971d9b6942dfdbe6b1bc35532a417a18c70f11
MariiaSemyhanovska <72863886+MariiaSemyhanovska@users.noreply.github.com>
2020-11-04 22:40:56 +0200

    Add files via upload

---
+++ JavaScript-task1/task1.js
@@ -0,0 +1,115 @@
+// 1. присвоить каждому из следующих значений свою переменную: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false Вывести каждую при помощи console.log , alert, document.write
+
+let greeting = "hello";
+let course = "owu";
+let intern_tld = "com";
+let local_tld = "ua";
+let task_number = 1;
+let lecture_amount = 10;
+let reverse_devil = -999;
+let order = 123;
+let pi = 3.14;
+let exam_grade = 2.7;
+let teen = 16;
+let work_hard = true;
+let sleep_well = false;
+
+console.log (greeting);
+alert (greeting);
+document.write (greeting);
+document.write ('<br>');
+
+console.log (course);
+alert (course);
+document.write (course);
+document.write ('<br>');
+
+console.log (intern_tld);
+alert (intern_tld);
+document.write (intern_tld);
+document.write ('<br>');
+
+console.log (local_tld);
+alert (local_tld);
+document.write (local_tld);
+document.write ('<br>');
+
+console.log (task_number);
+alert (task_number);
+document.write (task_number);
+document.write ('<br>');
+
+console.log (lecture_amount);
+alert (lecture_amount);
+document.write (lecture_amount);
+document.write ('<br>');
+
+console.log (reverse_devil);
+alert (reverse_devil);
+document.write (reverse_devil);
+document.write ('<br>');
+
+console.log (order);
+alert (order);
+document.write (order);
+document.write ('<br>');
+
+console.log (pi);
+alert (pi);
+document.write (pi);
+document.write ('<br>');
+
+console.log (exam_grade);
+alert (exam_grade);
+document.write (exam_grade);
+document.write ('<br>');
+
+console.log (teen);
+alert (teen);
+document.write (teen);
+document.write ('<br>');
+
+console.log (work_hard);
+alert (work_hard);
+document.write (work_hard);
+document.write ('<br>');
+
+console.log (sleep_well);
+alert (sleep_well);
+document.write (sleep_well);
+document.write ('<br>');
+
+//4. при помощи 3х разных prompt() получить 3 слова которые являются вашими ФИО. Для фамилии имени и отчества создать разные переменные. Вывести каждую при помощи console.log , alert, document.write
+let second_name = prompt("What is your last name?");
+let first_name = prompt("What is your first name?");
+let patronymic = prompt("What is your patronymic?");
+
+console.log (second_name);
+alert (second_name);
+document.write (second_name);
+document.write ('<br>');
+
+console.log (first_name);
+alert (first_name);
+document.write (first_name);
+document.write ('<br>');
+
+console.log (patronymic);
+alert (patronymic);
+document.write (patronymic);
+document.write ('<br>');
+
+// 13. - Поставьте соответствующий оператор в выражениях что бы получился соответсвующий результат.В одинаковых выражениях в задании не использовать два одинаковых оператора.Каждое выражение вложить в свою переменную
+     5 < 6 -> true
+     5 > 6 -> false
+     5 >= 6 -> false
+     5 == 6 -> false
+     10 == 10 -> true
+     10 === 10 -> true
+     10 > 10 -> false
+     10 < 10 -> false
+     10 !== 10 -> false
+     123 === '123' -> false
+     123 == '123' -> true
+
+    Каждое выражение вложить в свою переменную -  это вот так: let compare = (5 < 6)  ?
